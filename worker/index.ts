06d61b4535237fb24af2b86ec1fdcf46922d63b0
Wes Bos <wesbos@gmail.com>
2025-06-25 16:10:42 -0400

    provide clarification on 404 worker

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,8 @@
+
 export default {
   fetch(request) {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
+    // tHis file only exists to throw a 404, which causes it to fallback to static file via the `"not_found_handling": "single-page-application"` setting.
+    // I've done is so I can host a static app in a worker instead of using cloudflare pages.
 		return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
